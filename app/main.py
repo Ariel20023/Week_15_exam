a8b8b579ece91ae6ad47d76ed33bc560b54076c1
Ariel_Izralevitch <your.email@example.com>
2026-01-29 11:56:21 +0200

    yaml

--- app/main.py
+++ app/main.py
@@ -21,30 +21,86 @@ def customers_credit_limit_outliers():
     conn.close()
     return rows
 
-# @app.get("/q2/orders-null-comments")
-# def orders_null_comments():
-#     pass
-#
-# @app.get("/q3/customers-first-5")
-# def customers_first_5():
-#     pass
-#
-# @app.get("/q4/payments-total-average")
-# def payments_total_average():
-#     pass
-#
-# @app.get("/q5/employees-office-phone")
-# def employees_office_phone():
-#     pass
-#
-# @app.get("/q6/customers-shipping-dates")
-# def customers_shipping_dates():
-#     pass
-#
-# @app.get("/q7/customer-quantity-per-order")
-# def customer_quantity_per_order():
-#     pass
-#
-# @app.get("/q8/customers-payments-by-lastname-pattern")
-# def customers_payments_by_lastname_pattern(pattern: str = "son"):
-#     pass
+@app.get("/q2/orders-null-comments")
+def orders_null_comments():
+    conn = get_db_connection()
+    cursor = conn.cursor(dictionary=True)
+    cursor.execute(get_orders_with_null_comments())
+    rows = cursor.fetchall()
+
+    cursor.close()
+    conn.close()
+    return rows
+
+
+@app.get("/q3/customers-first-5")
+def customers_first_5():
+    conn = get_db_connection()
+    cursor = conn.cursor(dictionary=True)
+    cursor.execute(get_first_5_customers())
+    rows = cursor.fetchall()
+
+    cursor.close()
+    conn.close()
+    return rows
+
+
+@app.get("/q4/payments-total-average")
+def payments_total_average():
+    conn = get_db_connection()
+    cursor = conn.cursor(dictionary=True)
+    cursor.execute(get_payments_total_and_average())
+    rows = cursor.fetchall()
+
+    cursor.close()
+    conn.close()
+    return rows
+
+
+@app.get("/q5/employees-office-phone")
+def employees_office_phone():
+    conn = get_db_connection()
+    cursor = conn.cursor(dictionary=True)
+    cursor.execute(get_employees_with_office_phone())
+    rows = cursor.fetchall()
+
+    cursor.close()
+    conn.close()
+    return rows
+
+
+@app.get("/q6/customers-shipping-dates")
+def customers_shipping_dates():
+    conn = get_db_connection()
+    cursor = conn.cursor(dictionary=True)
+    cursor.execute(get_customers_with_shipping_dates())
+    rows = cursor.fetchall()
+
+    cursor.close()
+    conn.close()
+    return rows
+
+
+@app.get("/q7/customer-quantity-per-order")
+def customer_quantity_per_order():
+    conn = get_db_connection()
+    cursor = conn.cursor(dictionary=True)
+    cursor.execute(get_customer_quantity_per_order())
+    rows = cursor.fetchall()
+
+    cursor.close()
+    conn.close()
+    return rows
+
+
+@app.get("/q8/customers-payments-by-lastname-pattern")
+def customers_payments_by_lastname_pattern():
+    conn = get_db_connection()
+    cursor = conn.cursor(dictionary=True)
+    cursor.execute(get_customers_payments_by_lastname_pattern())
+    rows = cursor.fetchall()
+
+    cursor.close()
+    conn.close()
+    return rows
+
